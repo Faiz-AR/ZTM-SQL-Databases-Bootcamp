4c37b37725d10df18f414b947c36aad235a88fd7
Faiz <74257945+Faiz-AR@users.noreply.github.com>
2021-02-06 01:26:12 +0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -42,3 +42,56 @@ SELECT AVG(income) FROM customers WHERE age > 20 AND age < 50;
 
 SELECT age, income, country FROM customers WHERE (age < 30 OR age >= 50) AND (country = 'Japan' OR country = 'Australia') AND income > 50000;
 SELECT SUM(totalamount) FROM orders WHERE (orderdate >= '2004-06-01' AND orderdate <= '2004-06-30') AND totalamount > 100;
+
+-- Exercise: NULL Value Coalescing
+
+SELECT AVG(coalesce(age,15)) from "Student";
+SELECT id, coalesce(name, 'fallback'), coalesce(lastName, 'lastName'), age FROM "Student";
+
+-- Exercise: 3 Valued Logic
+
+SELECT COALESCE(address2,'No Address') FROM customers;
+SELECT * FROM customers WHERE address2 IS NOT null;
+SELECT coalesce(lastName, 'Empty'), * from customers where (age IS NULL);
+
+-- Exercise: BETWEEEN + AND
+
+SELECT age, income FROM customers WHERe income < 50000 AND (age BETWEEN 30 AND 50);
+SELECT AVG(income) FROM customers WHERe age BETWEEN 20 AND 50;
+
+-- Exercise: IN Keyword
+
+SELECT COUNT(orderid) FROM orders WHERE customerid IN (7888, 1082, 12808, 9623);
+SELECT COUNT(id) FROM city WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
+
+-- Exercise: LIKE Keyword
+
+SELECT emp_no, first_name, EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees WHERE first_name LIKE 'M%';
+SELECT COUNT(emp_no) FROM employees WHERE first_name ILIKE 'A%R';
+SELECT COUNT(customerid) FROM customers WHERE zip::text LIKE '%2%';
+SELECT COUNT(customerid) FROM customers WHERE zip::text LIKE '2_1%';
+SELECT COALESCE(state, 'No State'), phone FROM customers WHERE phone LIKE '302%';
+
+-- Exercise: Data and Timestamp
+
+SELECT AGE(birth_date), * FROM employees WHERE (EXTRACT (YEAR FROM AGE(birth_date))) > 60;
+SELECT COUNT(emp_no) FROM employees WHERE date_part('month', hire_date) = 02;
+SELECT COUNT(emp_no) FROM employees WHERE date_part('month', birth_date) = 11;
+SELECT MAX(AGE(birth_date)) FROM employees;
+SELECT COUNT(orderid) FROM orders WHERE DATE_TRUNC('month', orderdate) = date '2004-01-01';
+
+-- Exercise: Distinct Keyword
+
+SELECT DISTINCT(title) FROM titles;
+SELECT COUNT(DISTINCT(birth_date)) FROM employees;
+SELECT DISTINCT(lifeexpectancy) FROM country WHERE lifeexpectancy IS NOT NULL ORDER BY lifeexpectancy DESC;
+
+-- Exercise: Sorting Data
+
+SELECT first_name, last_name FROM employees ORDER BY first_name ASC, last_name DESC;
+SELECT AGE(birth_date),* FROM employees ORDER BY AGE(birth_date) DESC;
+SELECT * FROM employees WHERE first_name LIKE 'K%' ORDER BY hire_date DESC;
+
+-- Exercise: Inner Join
+
+
